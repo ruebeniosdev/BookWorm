29f811a628305c7dbef36d90cc5081fc514838d7
Akankobateng Rueben <akankobatengr@gmail.com>
2020-01-01 00:30:47 +0000

    Initial Commit

---
+++ BookWorm10/BookWorm10App.swift
@@ -0,0 +1,17 @@
+//
+//  BookWorm10App.swift
+//  BookWorm10
+//
+//  Created by RUEBEN on 1/1/20.
+//
+
+import SwiftUI
+
+@main
+struct BookWorm10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookWorm10/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BookWorm10
+//
+//  Created by RUEBEN on 1/1/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
